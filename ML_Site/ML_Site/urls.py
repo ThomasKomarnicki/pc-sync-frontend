46465ac845547bea09f460479c40b160c72aea75
Thomas Komarnicki <tkomarnicki@hanekedesign.com>
2016-04-04 13:58:21 -0400

    add url conf for pages

--- ML_Site/ML_Site/urls.py
+++ ML_Site/ML_Site/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from frontend import views;
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^help/$', views.help, name='help'),
+    url(r'^get-started/$', views.get_started, name='get started'),
+    url(r'^$', views.home, name='home'),
 ]
--- ML_Site/frontend/views.py
+++ ML_Site/frontend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    pass
+
+def help(request):
+    pass
+
+def get_started(request):
+    pass
