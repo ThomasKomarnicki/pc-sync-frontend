90a4c0fe061cf8abc698d94eab9b13138c0eacd9
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2016-04-25 21:11:38 -0400

    add headers for faq sections

--- ML_Site/frontend/views.py
+++ ML_Site/frontend/views.py
@@ -7,18 +7,24 @@ def home(request):
 
 
 def help(request):
-    faq = Faq.objects.order_by('order').all()
-    items = [
-    {'question': 'Why is the app never able to connect to my computer?',
-     'answer': 'Make sure your Android TV and Windows PC are on the same local network'},
+    # faq = Faq.objects.order_by('order').all()
+    sections = [
+        {
+            'title': 'Connection Issues',
+            'items': [
+                        {'question': 'Why is the app never able to connect to my computer?',
+                         'answer': 'Make sure your Android TV and Windows PC are on the same local network'},
 
-    {'question': 'Why is there a black screen when I try to play some videos?',
-     'answer': 'These videos may be improperly encoded and therefore not playable'},
+                        {'question': 'Why is there a black screen when I try to play some videos?',
+                         'answer': 'These videos may be improperly encoded and therefore not playable'},
+                    ]
+        }
     ]
+
     # for qa in faq:
     #     items.append({'question': qa.question, 'answer': qa.answer})
 
-    return render(request, 'help.html', context={'faq': items})
+    return render(request, 'help.html', context={'faq': sections})
 
 
 def get_started(request):
